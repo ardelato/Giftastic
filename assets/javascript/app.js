0b0481d678a6937d268585fefe17970f954d710d
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-02-27 22:59:30 -0800

    Got button - animal gif request to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,11 +2,8 @@
 var apiKEY = "dN47xDe3nAYUGnI6SP6L7vdcOCQFqXel";
 var baseURL = "https://api.giphy.com/v1/gifs/search?";
 
-var limit = 10; //limit
-var search = "cheeseburgers"; //q
-
-var queryURL =
-  baseURL + "api_key=" + apiKEY + "&limit=" + limit + "&q=" + search;
+var limit = 1; //limit
+//q
 
 var topics = ["eagle", "linx", "flamingo"];
 
@@ -18,24 +15,50 @@ function loadTopics() {
     newButton.appendTo(".list-container");
   });
 }
-$(window).on("load", function() {
-  loadTopics();
+
+function embedGifs(gifArray) {
+  gifArray.forEach((element) => {
+    var newDiv = $("<div>");
+    var newGif = $("<img>");
+    var newRating = $("<p>");
+
+    newRating.text(element.rating);
+    newGif.attr("src", element.images.original.url);
+    newDiv.append(newRating);
+    newDiv.append(newGif);
+    newDiv.appendTo(".gif-container");
+  });
+}
+function runQuery(animal) {
+  var queryURL =
+    baseURL + "q=" + animal + "&api_key=" + apiKEY + "&limit=" + limit;
+  console.log(animal);
+
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
     console.log(response);
+    embedGifs(response.data);
   });
+}
+
+$(window).on("load", function() {
+  loadTopics();
+  //Want new Animal
   $("form").submit(function(event) {
-    console.log(
-      $("input")
-        .first()
-        .val()
-    );
+    var newAnimal = $("input")
+      .first()
+      .val();
+    if (!topics.includes(newAnimal)) {
+      topics.push(newAnimal);
+      loadTopics();
+    }
     event.preventDefault();
   });
 });
 
 $(document).on("click", "button", function() {
   console.log($(this).text());
+  runQuery($(this).text());
 });
